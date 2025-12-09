c304190918c0ce2d31fc21dcda49f11a6aa0a600
domer <you@example.com>
2025-12-09 22:21:14 +0800

    update

--- src/app/main.py
+++ src/app/main.py
@@ -38,6 +38,7 @@ template_dir = os.path.join(base_dir, 'templates')
 app = Flask(__name__, template_folder=template_dir)
 app.secret_key = os.urandom(24) 
 
+# bot token
 TOKEN = os.getenv("bot_token")
 
 @bot.event
